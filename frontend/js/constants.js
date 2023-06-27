2b72c0f244fdd1055ab98529f516404fc3448ad4
Morris <mourymacharia@gmail.com>
2023-06-27 14:12:30 -0400

    message

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x3E1026Afdb441cdd991919F800954Bccc87615e8"; // Replace with your own contract address
 const chain = 'goerli'; // goerli, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
